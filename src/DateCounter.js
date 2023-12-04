d53f9526dfb07cac442e59dc190335f492f6d708
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-03 23:56:24 -0600

    in progress

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import {useReducer, useState} from "react";
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
