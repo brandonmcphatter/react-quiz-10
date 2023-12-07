89d257164b6963cb0254d6f8bd7d7cab945a9633
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-06 20:52:01 -0600

    created fake api and fetched questions from it with useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,53 @@
 import './App.css';
 import Header from "./Header";
 import Main from './Main';
+import {useEffect, useReducer} from "react";
 
-function App() {
-  return (
-    <div className="app">
-        <Header/>
-        <Main>
-            <p>1/15</p>
-            <p>Question?</p>
-        </Main>
+const initialState = {
+    questions: [],
+    status: 'loading', // loading, ready, active, finished, error
+    currentQuestion: 0,
+    answers: [],
+    score: 0
+};
 
-
-    </div>
-  );
+function reducer(state, action) {
+    switch (action.type) {
+        case 'dataReceived':
+            return {
+                ...state,
+                questions: action.payload,
+                status: 'ready'
+            };
+        case 'dataFailed':
+            return {
+                ...state,
+                status: 'error'
+            };
+    }
 }
 
-export default App;
+export default function App() {
+    const [state, dispatch] = useReducer(reducer, initialState, undefined);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/questions')
+            .then(res => res.json())
+            .then(data => dispatch({type: 'dataReceived', payload: data}))
+            .catch(() => dispatch({type: 'dataFailed'}));
+
+    }, []);
+
+    return (
+        <div className="app">
+            <Header/>
+            <Main>
+                <p>1/15</p>
+                <p>{state.questions[1].question}</p>
+            </Main>
+
+
+        </div>
+    );
+}
+
