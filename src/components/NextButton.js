b987bae1279b10227d1d16f22439716d7f8f3d67
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-07 19:44:25 -0600

    added conditional for when to show next button or finish button

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function NextButton({dispatch, answer}) {
+function NextButton({dispatch, answer, numbQuestions, index}) {
     if (answer === null) return null;
-    return (
+    if (index < numbQuestions - 1) return (
         <button className='btn btn-ui'
         onClick={()=> dispatch({type: 'next'})}>
             Next
+        </button>
+    );
+    return (
+        <button className='btn btn-ui'
+        onClick={()=> dispatch({type: 'finish'})}>
+            Finish
         </button>
     );
 }
