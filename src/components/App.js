a37e6f4fdae39a259b83baba56a0339d687b9a1e
Brandon McPhatter <brandonmcphatter1@gmail.com>
2023-12-30 14:21:30 -0600

    reducer / functionality moved to context

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import '../App.css';
 import Header from "./Header";
 import Main from './Main';
-import {useEffect, useReducer} from "react";
 import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
@@ -12,145 +11,42 @@ import FinishScreen from "./FinishScreen";
 import ResetButton from "./ResetButton";
 import Footer from "./Footer";
 import Timer from "./Timer";
-
-const SEC_PER_QUESTION = 30;
-
-const initialState = {
-    questions: [],
-    status: 'loading', // loading, ready, active, finished, error
-    index: 0,
-    answer: null,
-    score: 0,
-    highScore: 0,
-    secRemaining: 10
-};
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'dataReceived':
-            return {
-                ...state,
-                questions: action.payload,
-                status: 'ready'
-            };
-        case 'dataFailed':
-            return {
-                ...state,
-                status: 'error'
-            };
-        case 'start':
-            return {
-                ...state,
-                status: 'active',
-                secRemaining: state.questions.length * SEC_PER_QUESTION
-            };
-        case 'newAnswer':
-            const question = state.questions[state.index];
-            return {
-                ...state,
-                answer: action.payload,
-                score: action.payload === question.correctOption
-                    ? state.score + question.points
-                    : state.score
-            };
-        case 'next':
-            return {
-                ...state,
-                index: state.index + 1,
-                answer: null
-            };
-        case 'finish':
-            return {
-                ...state,
-                status: 'finished',
-                highScore: state.score > state.highScore ? state.score : state.highScore
-            };
-        case 'restart':
-            return {
-                ...initialState,
-                questions: state.questions,
-                status: 'ready'
-            };
-        case 'tick':
-            return {
-                ...state,
-                secRemaining: state.secRemaining - 1,
-                status: state.secRemaining === 0 ? 'finished' : state.status
-            };
-
-        default:
-            throw new Error('Action not supported');
-    }
-}
+import {useQuiz} from "../contexts/QuizContext";
 
 export default function App() {
-    const [{
-        questions,
-        status,
-        index,
-        answer,
-        score,
-        highScore,
-        secRemaining
-    }, dispatch] = useReducer(reducer, initialState, undefined);
-    const numbQuestions = questions.length;
-    const totalScore = questions.reduce((acc, question) => acc + question.points, 0);
+    const {index, status, questions} = useQuiz();
 
-    useEffect(() => {
-        fetch('http://localhost:3001/questions')
-            .then(res => res.json())
-            .then(data => dispatch({type: 'dataReceived', payload: data}))
-            .catch(() => dispatch({type: 'dataFailed'}));
 
-    }, []);
 
     return (
-        <div className="app">
-            <Header/>
 
-            <Main>
-                {status === 'loading' && <Loader/>}
+            <div className="app">
+                <Header/>
+                <Main>
+                    {status === 'loading' && <Loader/>}
+                    {status === 'error' && <Error/>}
+                    {status === 'ready' && <StartScreen/>}
+                    {status === 'active' &&
+                        <>
+                            <Progress/>
+                            <Question question={questions[index]}/>
 
-                {status === 'error' && <Error/>}
+                            <Footer>
+                                <Timer/>
+                                <NextButton index={index}/>
+                            </Footer>
+                        </>}
 
-                {status === 'ready' && <StartScreen numbQuestions={numbQuestions} dispatch={dispatch}
-                />}
+                    {status === 'finished' &&
+                        <>
+                            <FinishScreen/>
 
-                {status === 'active' &&
-                    <>
-                        <Progress numbQuestions={numbQuestions}
-                                  index={index}
-                                  score={score}
-                                  answer={answer}
-                                  totalScore={totalScore}/>
-                        <Question question={questions[index]}
-                                  dispatch={dispatch}
-                                  answer={answer}/>
+                            <ResetButton/>
+                        </>}
 
-                        <Footer>
-                            <Timer dispatch={dispatch}
-                                   secRem={secRemaining}/>
-                            <NextButton answer={answer}
-                                        dispatch={dispatch}
-                                        index={index}
-                                        numbQuestions={numbQuestions}/>
-                        </Footer>
-                    </>}
+                </Main>
+            </div>
 
-                {status === 'finished' &&
-                    <>
-                        <FinishScreen score={score}
-                                      totalScore={totalScore}
-                                      dispatch={dispatch}
-                                      highScore={highScore}/>
-
-                        <ResetButton dispatch={dispatch}/>
-                    </>}
-
-            </Main>
-
-
-        </div>
     );
 }
 
